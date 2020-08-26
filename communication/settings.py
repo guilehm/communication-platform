5af7cf07ae8463e07c3712ef1f671cc92d5bf754
guilherme <guile.hm@hotmail.com>
2020-08-25 22:07:16 -0300

    install and configure django rest framework and drf-yasg

--- communication/settings.py
+++ communication/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'communication.core',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'COERCE_DECIMAL_TO_STRING': False,
+}
